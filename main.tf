8e65ac02d29c461038a4652b865836d35a545e5e
= <=>
2024-07-02 21:33:41 -0300

    Atualizando o arquivo hosts.yml e playbook.yml

--- main.tf
+++ main.tf
@@ -18,13 +18,6 @@ resource "aws_instance" "app_server_rafael" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
   key_name ="chave_ssh"
-  user_data = <<-EOF
-                 #!/bin/bash
-                 cd /home/ubuntu
-                 echo "<h1>Feito com Terraform</h1>" > index.html
-                 nohup busybox httpd -f -p 8080 &
-                 EOF
-  
   tags = {
     Name = "Primeira instância Rafael"
   }
