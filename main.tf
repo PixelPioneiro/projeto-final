29d6ed5bcc618a522f2454dce4e8f815cb2fb3d3
= <=>
2024-07-02 21:09:53 -0300

    Arquivos atualizados

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 resource "aws_instance" "app_server_rafael" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
-  key_name ="projeto_final"
+  key_name ="chave_ssh"
   user_data = <<-EOF
                  #!/bin/bash
                  cd /home/ubuntu
